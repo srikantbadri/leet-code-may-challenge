29cc834ad2145128ac99075f50e1acd7565b06d4
Srikant Badri <srikantbadri96@gmail.com>
2020-05-10 20:06:05 +0530

    Update and rename Day6.java to 6_Majority_Element.java

--- 6_Majority_Element.java
+++ 6_Majority_Element.java
@@ -1,3 +1,4 @@
+//O(NlogN) solution through sorting
 class Solution {
     public int majorityElement(int[] nums) {
         int len=nums.length;
@@ -19,5 +20,28 @@ class Solution {
             maxIndex=len-1;
         }
         return nums[maxIndex];
+    }
+}
+
+
+
+//Majority Element
+//Boyer Moore Majority Vote Algorithm
+//O(n) Complexity
+class Solution {
+    public int majorityElement(int[] nums) {
+        int counter=0;
+        int majorityElement=-1;
+        for(int num:nums){
+            if(counter == 0){
+                counter=1;
+                majorityElement=num;
+            }else if(majorityElement == num){
+                counter+=1;
+            }else{
+                counter-=1;
+            }
+        }
+        return majorityElement;
     }
 }
